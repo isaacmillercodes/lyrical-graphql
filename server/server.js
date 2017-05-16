fca6539f0608a4d51fc33bebc0e7ad8faf58fd60
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-05-16 12:33:40 -0600

    connected mongodb/mlab

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://isaac:happybirthday@ds143071.mlab.com:43071/im_lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
